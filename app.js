57f58b73d2e701c1253f0b9b1b60464d852aa626
nozdev <nozdev@gmail.com>
2019-12-14 23:12:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const signalR = require('./signalr')(http);
-const port = process.env.PORT||3000;
+const port = process.env.PORT;
 
 const chat = signalR.mapHub('/chat');
 const clock = signalR.mapHub('/clock');
